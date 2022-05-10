6d28bba312a8e33695453882a7ec4961c8bbe84b
vishnu244 <vishnunali244@gmail.com>
2022-05-10 17:52:38 +0530

    [Vishnu]-append elements in LinkedList

--- LinkedListMethods/LLMethods.cs
+++ LinkedListMethods/LLMethods.cs
@@ -9,7 +9,7 @@ namespace LinkedListMethods
     public class LLMethods<T>
     {
         public int count = 0;
-        Node<T> head;
+        public Node<T> head;
         
                    
         public void AddNodeToFront(T data)
--- LinkedListMethods/Program.cs
+++ LinkedListMethods/Program.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Types of Linked List Methods!");
 
 while (true)
 {
-    Console.WriteLine("Select the Program to be Executed :\n1)Add from Start\n2)Add From End");
+    Console.WriteLine("Select the Program to be Executed :\n1)Add from Start\n2)Add From End\n3)Append Method");
     int Option = Convert.ToInt32(Console.ReadLine());
     switch (Option)
     {
@@ -32,6 +32,21 @@ while (true)
             addmethod.display();
             Console.WriteLine("\n");
             break;
+        case 3:
+            LLMethods<int> appendmethod = new();
+            appendmethod.head = new Node<int>(56);
+            Node<int> newNode1 = new Node<int>(30);
+            Node<int> newNode2 = new Node<int>(70);
+            appendmethod.head.next = newNode1;
+            newNode1.next = newNode2;
+            newNode2.next = null;
+            Console.WriteLine("The elements in LinkedList are:");
+            appendmethod.display();
+            Console.WriteLine("\n");
+
+
+            break;
+
         default:
             Console.WriteLine("please choose correct option!");
             break;
